7fb2bc4f91b40c921e623436b4b427748a4e0d62
msernio <s4095318@student.rmit.edu.au>
2026-05-16 14:08:59 +1000

    Update

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const video = document.querySelector("#custom-audio-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
+const themeTitle = document.querySelector("#theme-title");
+const albumCover = document.querySelector(".album-cover");
 video.removeAttribute("controls");
+
 // playPauseBtn.addEventListener("click", togglePlayPause);
 video.addEventListener("timeupdate", updateProgressBar);
 function togglePlayPause() {
   if (video.paused || video.ended) {
     video.play();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+    playPauseImg.src = "Pause.png";
   } else {
     video.pause();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    playPauseImg.src = "Play.png";
   }
 }
 function updateProgressBar() {
@@ -19,4 +22,33 @@ function updateProgressBar() {
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
-h
+
+document.querySelector("#theme-forest").addEventListener("click", function() {
+    themeTitle.textContent = "Forest";
+    albumCover.src = "Image1.jpeg";
+    video.src = "Forest.mp3";
+    video.load();
+});
+
+document.querySelector("#theme-Rain").addEventListener("click", function() {
+    themeTitle.textContent = "Rain";
+    albumCover.src = "Image2.jpeg";
+    video.src = "Rain.mp3";
+    video.load();
+});
+
+document.querySelector("#theme-Night").addEventListener("click", function() {
+    themeTitle.textContent = "Night";
+    albumCover.src = "Image3.jpeg";
+    video.src = "Night.mp3";
+    video.load();
+});
+
+document.querySelector("#skip-back-btn").addEventListener("click", function() {
+  video.currentTime = video.currentTime - 15;
+});
+
+document.querySelector("#skip-forward-btn").addEventListener("click", function() {
+  video.currentTime = video.currentTime + 15;
+});
+
